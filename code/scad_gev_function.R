daf4985ef492751e604d2912c0a098fcd10a2187
Kyucheol Eom <ekc0106@naver.com>
2019-12-03 11:14:50 +0900

    Add files via upload

---
+++ code/scad_gev_function.R
@@ -0,0 +1,185 @@
+# update function
+if(!require(Deriv)){install.packages("Deriv")};library(Deriv)
+if(!require(evd)){install.packages("evd")};library(evd)
+
+#loss function
+# logl=expression(log(sigma)+(1+1/k)*log(1+k*(x-mu)/sigma)+(1+k*(x-mu)/sigma)^(-1/k))
+# func_loss = function()
+
+#z_function
+func_z <- function(dmatrix, mu, u, lam, rho, a){
+  
+  for(i in 1:length(z_init)){
+    if(abs(z_init[i]) < lam){
+      if(abs(dmatrix %*% mu + u)[i] <= (rho + 1)*lam/rho & abs(dmatrix %*% mu + u)[i] >= lam/rho){
+        z_init[i] <- (dmatrix %*% mu + u)[i] - sign(dmatrix %*% mu + u)[i] * lam/rho
+      }else if(abs(dmatrix %*% mu + u)[i] < lam/rho){
+        z_init[i] <- 0
+      }else{
+        z_init[i] <- sign(dmatrix %*% mu + u)[i] * lam
+      }
+    }else if( abs(z_init[i]) > lam & abs(z_init[i]) <= a*lam ){
+      if((rho*a - rho)/(rho*a - rho - 1) * abs(dmatrix %*% mu + u)[i] >= a/(rho*a - rho - 1) - a & 
+         (rho*a - rho)/(rho*a - rho - 1) * abs(dmatrix %*% mu + u)[i] < a/(rho*a - rho - 1) - 1){
+        z_init[i] <- (rho*a - rho)/(rho*a - rho - 1) * (dmatrix %*% mu + u)[i] -sign(dmatrix %*% mu)[i]* a*lam/(rho*a - rho - 1)
+      }else if((rho*a - rho)/(rho*a - rho - 1) * abs(dmatrix %*% mu + u)[i] > a/(rho*a - rho - 1) - 1){
+        z_init[i] <- -lam*sign(dmatrix %*% mu + u)[i]
+      }else{
+        z_init[i] <- -a*lam*sign(dmatrix %*% mu + u)[i]
+      }
+    }else{
+      if(abs(dmatrix %*% mu + u) > a * lam){
+        z_init[i] <- (dmatrix %*% mu + u)[i]
+      }else{
+        c(a*lam, -a*lam)[which.min(c((dmatrix %*% mu - a*lam + u)[i],(dmatrix %*% mu + a*lam + u)[i]))]
+      }
+    }
+  }
+  return(z_init)
+}
+
+
+
+#u_function
+func_u <- function(dmatrix,mu,z,u) {
+  u <- u + (dmatrix %*% mu) - z
+  return(u)
+}
+
+#dmatrix
+mat_func = function(n) {
+  m = matrix(0,n-1,n)   ## dmatrix : (n-2)*n 
+  for (i in 1:(n-1)) {
+    m[i,i] = -1
+    m[i,i+1] = 1
+  }
+  return(m)
+}
+
+## gevfunction 
+gevreg = function(x, z, ctr_list)
+{
+  
+  l2gev = function (tvec, dmatrix, rho, z_init, u_init)
+  {
+    # loc.vec = zz%*%tvec[1:(p+1)]
+    loc.vec = tvec[1:n]
+    # loglikelihood
+    v1 = - sum(lgev(x, loc = loc.vec, scale = tvec[n+1], shape = tvec[n+2]))
+    
+    # lagrangian term
+    v2 = (rho/2)*sum(((dmatrix %*% loc.vec) - z_init + u_init)^2)
+    v = v1 + v2
+    return(v)
+  }
+  
+  lgev = function (x, loc = 0, scale = 1, shape = 0) 
+  {
+    if (min(scale) <= 0) 
+      return( - 1e+6)
+    if (length(shape) != 1) 
+      stop("invalid shape")
+    x <- (x - loc)/scale
+    if (shape == 0) 
+      d <- log(1/scale) - x - exp(-x)
+    else {
+      nn <- length(x)
+      xx <- 1 + shape * x
+      xxpos <- xx[xx > 0 | is.na(xx)]
+      scale <- rep(scale, length.out = nn)[xx > 0 | is.na(xx)]
+      d <- numeric(nn)
+      d[xx > 0 | is.na(xx)] <- log(1/scale) - xxpos^(-1/shape) - 
+        (1/shape + 1) * log(xxpos)
+      d[xx <= 0 & !is.na(xx)] <- -(1e+6)
+    }
+    return(d)
+  }
+  
+  est = optim(tvec, l2gev, dmatrix = dmatrix,
+              rho=init_rho, z_init = z_init, u_init = u_init, method = "BFGS",
+              control = ctr_list)$par
+  
+  
+  return(est)
+}
+## refit function
+
+gevreg_m = function(tvec = tvec, x = x, zmat = zmat)
+{
+  p = ncol(zmat)
+  l2gev_m = function (tvec = tvec, x = x, zmat = zmat)
+  {
+    # ns = length(xlist)
+    # v1 = 0
+    # for ( i in 1: length(xlist))
+    # {
+    loc.vec.reg = drop(zmat%*%tail(tvec, p))
+    v = -sum(lgev(x, loc = loc.vec.reg, # v1이 필요없음.
+                  scale = tvec[1], shape = tvec[2]))   # loss : +(-loglikelihood)
+    # }
+    # loglikelihood
+    # regularization
+    
+    
+    return(v)
+  }
+  
+  lgev = function (x, loc = 0, scale = 1, shape = 0) 
+  {
+    if (min(scale) <= 0) 
+      return( - 1e+6)
+    if (length(shape) != 1) 
+      stop("invalid shape")
+    x <- (x - loc)/scale
+    if (shape == 0) 
+      d <- log(1/scale) - x - exp(-x)
+    else {
+      nn <- length(x)
+      xx <- 1 + shape * x
+      xxpos <- xx[xx > 0 | is.na(xx)]
+      scale <- rep(scale, length.out = nn)[xx > 0 | is.na(xx)]
+      d <- numeric(nn)
+      d[xx > 0 | is.na(xx)] <- log(1/scale) - xxpos^(-1/shape) - 
+        (1/shape + 1) * log(xxpos)
+      d[xx <= 0 & !is.na(xx)] <- -(1e+6)
+    }
+    return(d)
+  }
+  
+  # tmp_loc = 0
+  # for ( i in 1:ns)
+  # {
+  #   x = xlist[[i]]
+  #   fit = fgev(x)
+  #   tmp_loc = tmp_loc + fit$estimate[1]
+  #   tvec[2*i] = fit$estimate[2]
+  #   tvec[2*i+1] = fit$estimate[3]
+  # }
+  # tmp_loc = tmp_loc/ns
+  # tvec[1] = tmp_loc
+  
+  fit = optim(tvec, l2gev_m, method = "BFGS", 
+              x = x, zmat = zmat)
+  return(fit) 
+}
+lgev = function (x, loc = 0, scale = 1, shape = 0) 
+{
+  if (min(scale) <= 0) 
+    return( - 1e+6)
+  if (length(shape) != 1) 
+    stop("invalid shape")
+  x <- (x - loc)/scale
+  if (shape == 0) 
+    d <- log(1/scale) - x - exp(-x)
+  else {
+    nn <- length(x)
+    xx <- 1 + shape * x
+    xxpos <- xx[xx > 0 | is.na(xx)]
+    scale <- rep(scale, length.out = nn)[xx > 0 | is.na(xx)]
+    d <- numeric(nn)
+    d[xx > 0 | is.na(xx)] <- log(1/scale) - xxpos^(-1/shape) - 
+      (1/shape + 1) * log(xxpos)
+    d[xx <= 0 & !is.na(xx)] <- -(1e+6)
+  }
+  return(d)
+}
